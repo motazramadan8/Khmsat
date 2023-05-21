70cd5fec4948696f843efd4747ad82ad4489230c
Motaz Ramadan <120125123+motazramadan8@users.noreply.github.com>
2023-05-21 17:06:51 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+
+  var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+$(document).ready(function() {
+
+	$(".fa-search").click(function() {
+	   $(".togglesearch").toggle();
+	   $("input[type='text']").focus();
+	 });
+
+});
